5f57bf9646f0ad3eaa75790a8a126be99bb9ce7b
Américo <americo.200422@gmail.com>
2024-09-03 14:45:46 -0300

    atualização no componente button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,4 +1,10 @@
-const Button = ({ text }) => {
+import React from 'react';
+
+interface ButtonProps {
+  text: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ text }) => {
   return (
     <button
       className={`bg-orange-600 px-6 py-2 rounded-sm hover:bg-orange-700 duration-300 text-lg text-slate-200`}
